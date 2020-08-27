16481d122d87c5c637f11ee8b06a40969f05f96b
Farschad Saberi <fasa@nb-saberi.ad.bbs-bassgeige.eu>
2020-08-27 13:01:47 +0200

    Testing QSplashScreen.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "fshkwindow.h"
 #include <QApplication>
 #include <QGuiApplication>
+#include <QSplashScreen>
 
 int main(int argc, char *argv[])
 {
@@ -9,9 +10,21 @@ int main(int argc, char *argv[])
     QGuiApplication::setApplicationName("FSHK-AP");
     QGuiApplication::setApplicationVersion("1.0");
 
+    QPixmap pix = QPixmap(":/FSHKIcon.svg");
+
+    // testing
+    QSplashScreen *splash = new QSplashScreen (pix);
+    splash->show();
+
+    splash->showMessage("FSHK-AP startet...lade Module");
+    app.processEvents();
+
+    splash->showMessage("FSHK-AP Module sind geladen!");
+    app.processEvents();
 
     FSHKWindow window;
     window.show();
+    splash->finish(&window);
 
     return app.exec();
 }
