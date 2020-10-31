b4560a54f20134eccb79a9d9c315c6d29390a14d
Farschad Saberi <fasa@nb-saberi.ad.bbs-bassgeige.eu>
2020-10-31 09:40:58 +0100

    Fix printing page.

--- classprinting.cpp
+++ classprinting.cpp
@@ -3,13 +3,19 @@
 #include <QMessageBox>
 #include <QPageLayout>
 #include <QPainter>
+#include <QtMath>
+#include <fshkwindow.h>
+
+#include <QDebug>
 
 ClassPrinting::ClassPrinting(QObject *parent) : QObject(parent)
 {
-
+    m_project = ClassProjekt();
+    currentPage = 1;
+    maxPage = 1;
 }
 
-bool ClassPrinting::setupPrinter()
+bool ClassPrinting::setupPrinter(QPageLayout::Orientation layout)
 {
     QPrintDialog printDialog(&printer, nullptr);
     if(printDialog.exec() == QDialog::Rejected )
@@ -19,15 +25,14 @@ bool ClassPrinting::setupPrinter()
     }
 
     QPageLayout pageLayout = printer.pageLayout();
-    qreal top = pixel(15, printer);
-    qreal left = pixel(20, printer);
-    qreal right = pixel(15, printer);
-    qreal bottom = pixel(30, printer);
-    QMarginsF ma( left, top, right, bottom);
-    margin = ma;
-    pageLayout.setOrientation(QPageLayout::Portrait);
+    pageLayout.setOrientation( layout /*QPageLayout::Portrait*/);
     pageLayout.setPageSize(QPageSize(QPageSize::A4));
-    pageLayout.setMargins(margin);
+    pageLayout.setUnits(QPageLayout::Millimeter);
+    pageLayout.setMode(QPageLayout::FullPageMode);
+    pageLayout.setLeftMargin(15);
+    pageLayout.setTopMargin(10);
+    pageLayout.setRightMargin(10);
+    pageLayout.setBottomMargin(15);
     printer.setPageLayout( pageLayout );
 
     return true;
@@ -86,10 +91,6 @@ void ClassPrinting::setDescription(const QString &value)
     description = value;
 }
 
-void ClassPrinting::setColumn(int nr, qreal width)
-{
-    columnMap.insert(nr, width);
-}
 
 bool ClassPrinting::print()
 {
@@ -98,30 +99,88 @@ bool ClassPrinting::print()
     if(!painter.begin(&printer))
         return false;
 
+    setFontAttribute(10, 1.0,false, QColor(0,0,0), &painter);
+    setupRowMap(painter);
 
     // Print the frames and the minimum nesseccery text
-    setFontAttribute(1, false, Qt::lightGray, &painter);
-    painter.drawRect( headerFrame() );
-    painter.drawRect( pageFrame() );
-    painter.drawRect( feetFrame() );
+    setFontAttribute(10, 1, false, Qt::darkGray, &painter);
+    printPageFrames(&painter);
+    printPageInfoText(&painter);
 
+    // Print head project information
+    if(getProject().isValid())
+    {
+        // Calculate the page count
+        int rowNeeded = calculateRows( textList( getProject() ), 519, painter);
+        rowNeeded += 8; // 8 = Head rows and last rows for total result
+        maxPage = qCeil( rowNeeded / rowMap.size());
+        maxPage++;
 
-    setFontAttribute(14, false, QColor(77, 121, 255), &painter);
-    painter.drawText( QPointF( headerFrame().x() + 10, headerFrame().bottom()  - ( textHeight(painter) / 2) ) , getCaption() );
+        setFontAttribute(10, 1.0, false, QColor(0,0,0), &painter);
+        printProjectInfoText(&painter);
+        printProjectText(&painter);
+    }
 
-    setFontAttribute(12, false, QColor(0,0,0), &painter);
-    painter.drawText( QPointF( headerFrame().right() - textWidth(getCurrentDate() , painter) - 10,
-                               headerFrame().bottom()  - ( textHeight(painter) / 2) ), getCurrentDate() );
-
-    setFontAttribute(8, false, Qt::lightGray, &painter);
-    painter.drawText( QPointF( feetFrame().x() + 10, feetFrame().bottom() - ( textHeight(painter) / 2) ) , getDocName() );
-
-
-
+    // Print apprentice list
+    if(!getApprenticeList().isEmpty())
+    {
+        setFontAttribute(10, 1, false, QColor(0,0,0), &painter);
+        printApprenticeList(&painter);
+    }
 
     painter.end();
 
     return true;
+}
+
+ClassProjekt ClassPrinting::getProject() const
+{
+    return m_project;
+}
+
+void ClassPrinting::setProject(const ClassProjekt &project)
+{
+    m_project = project;
+}
+
+QString ClassPrinting::getFilepath() const
+{
+    return filepath;
+}
+
+void ClassPrinting::setFilepath(const QString &value)
+{
+    filepath = value;
+}
+
+void ClassPrinting::setupRowMap(const QPainter &p)
+{
+    qreal th = textHeight(p) * 1.5;
+    int rows = qFloor( pageFrame().height() / th);
+
+    qreal posY = pageFrame().top() + pixel(5);
+    qreal posX = pageFrame().left() + pixel(3);
+
+    for(int i = 0; i < rows; i++)
+    {
+        QPointF pos;
+        pos.setX(posX);
+        pos.setY(posY);
+        rowMap.insert(i, pos);
+
+        posY += th;
+        pos.setY(posY);
+    }
+}
+
+QList<ClassLehrling> ClassPrinting::getApprenticeList() const
+{
+    return m_apprenticeList;
+}
+
+void ClassPrinting::setApprenticeList(const QList<ClassLehrling> &apprenticeList)
+{
+    m_apprenticeList = apprenticeList;
 }
 
 QString ClassPrinting::getDocName() const
@@ -132,6 +191,11 @@ QString ClassPrinting::getDocName() const
 qreal ClassPrinting::pageLeft()
 {
     return printer.pageRect().left();
+}
+
+qreal ClassPrinting::pageRight()
+{
+    return printer.pageRect().right();
 }
 
 qreal ClassPrinting::pageTop()
@@ -146,44 +210,46 @@ qreal ClassPrinting::pageBottom()
 
 qreal ClassPrinting::pageWidth()
 {
-    return printer.pageRect().width() - margin.left() - margin.right();
+    QPageLayout layout = printer.pageLayout();
+    int ml = layout.marginsPixels(printer.resolution()).left();
+    int mr = layout.marginsPixels(printer.resolution()).right();
+    qreal w = printer.pageRect().width() - ml - mr;
+
+    return w;
 }
 
 qreal ClassPrinting::pageHeight()
 {
-    return printer.pageRect().height() - margin.top()- margin.bottom();
+    QPageLayout layout = printer.pageLayout();
+    int mt = layout.marginsPixels(printer.resolution()).top();
+    int mb = layout.marginsPixels(printer.resolution()).bottom();
+    qreal h = printer.pageRect().height() - mt - mb;
+
+    return h;
 }
 
-QRectF ClassPrinting::headerFrame()
+QRectF ClassPrinting::pageRect()
 {
-    QRectF rect(pageLeft(), pageTop(), pageWidth(), pixel(45, printer));
+    QRectF rect(pageLeft(), pageTop(), pageWidth(), pageHeight());
+    return rect;
+}
+
+QRectF ClassPrinting::captionFrame()
+{
+    QRectF rect(0, 0, pageWidth(), pixel(10));
     return rect;
 }
 
 QRectF ClassPrinting::feetFrame()
 {
-    QPointF p1;
-    p1.setX( pageLeft());
-    p1.setY(pageHeight());
-
-    QRectF rect(p1.x(), p1.y(), pageWidth(), pixel(25, printer));
+    QRectF rect(0, pageHeight()-pixel(5), pageWidth(), pixel(5));
     return rect;
 }
 
 QRectF ClassPrinting::pageFrame()
 {
-
-    QPointF p1;
-    p1.setX( headerFrame().bottomLeft().x() );
-    p1.setY( headerFrame().bottomLeft().y()+pixel(10,printer));
-
-    QPointF p2;
-    p2.setX( feetFrame().topLeft().x() );
-    p2.setY( feetFrame().topLeft().y()-pixel(10,printer));
-
-    qreal h = QLineF(p1, p2).length();
-    qreal w = headerFrame().width();
-    QRectF rect(p1.x(), p1.y(), w, h);
+    qreal h = pageHeight() - captionFrame().height() - pixel(3) - feetFrame().height() - pixel(3);
+    QRect rect(0, captionFrame().bottom() + pixel(3), pageWidth(), h );
     return rect;
 }
 
@@ -197,27 +263,26 @@ QString ClassPrinting::getText() const
     return text;
 }
 
-qreal ClassPrinting::pixel(qreal millimeter, const QPrinter &p)
+qreal ClassPrinting::pixel(qreal millimeter)
 {
-    qreal fac = p.pageRect().width() / p.pageRect(QPrinter::Millimeter).width();
-    int dpi = p.resolution();
+    int dpi = printer.resolution();
     qreal pixelPerMM = dpi / 25.4;
 
-    return fac * millimeter / pixelPerMM;
+    return millimeter * pixelPerMM;
 }
 
-void ClassPrinting::setFontAttribute(int size, bool bold, QColor col, QPainter *p)
+void ClassPrinting::setFontAttribute(int fontSize, qreal penWidth, bool bold, QColor col, QPainter *p)
 {
     QFont font = p->font();
     QPen pen = p->pen();
 
-    font.setPointSize(size);
+    font.setPointSize(fontSize);
     font.setBold(bold);
     pen.setColor(col);
+    pen.setWidth(penWidth);
 
     p->setFont(font);
     p->setPen(pen);
-
 }
 
 qreal ClassPrinting::textHeight(const QPainter &p)
@@ -232,4 +297,381 @@ qreal ClassPrinting::textWidth(const QString &text, const QPainter &p)
     return fm.width(text);
 }
 
+void ClassPrinting::setPenSize(qreal size, QPainter *p)
+{
+    QPen pen = p->pen();
+    pen.setWidthF(size);
+    p->setPen(pen);
+}
 
+QStringList ClassPrinting::splitText(const QString &text, qreal maxLength, const QPainter &p)
+{
+    QStringList list = text.split(" ");
+
+    QStringList textList;
+    QString string = "";
+
+    foreach (QString s, list)
+    {
+        if( textWidth(string+s+" ", p) < maxLength)
+        {
+            string.append(s);
+            string.append(" ");
+
+        }
+        else
+        {
+            textList << string;
+            string.clear();
+            string.append(s);
+            string.append(" ");
+        }
+    }
+
+    textList << string;
+
+
+    return textList;
+}
+
+int ClassPrinting::calculateRows(const QStringList &textList, qreal rowWidth, const QPainter &p)
+{
+    int rows = 0;
+    foreach (QString s, textList)
+    {
+        if( textWidth(s, p) > rowWidth)
+            rows += splitText(s, rowWidth, p).size();
+        else
+            rows++;
+    }
+
+    return rows;
+}
+
+QStringList ClassPrinting::textList(const ClassProjekt &pro)
+{
+    QStringList list;
+    foreach (ClassFrage q, pro.questionMap().values())
+    {
+        QString s = "100:";
+        s.append(q.question());
+        list << s;
+    }
+    return list;
+}
+
+void ClassPrinting::printPageFrames(QPainter *p)
+{
+    p->drawRect( captionFrame() );
+    p->drawRect( pageFrame() );
+    p->drawRect( feetFrame() );
+}
+
+void ClassPrinting::printPageInfoText(QPainter *p)
+{
+    // Print caption and date
+    setFontAttribute(14, 1.0, false, QColor(77, 121, 255), p);
+    p->drawText( QPointF( captionFrame().x() + pixel(3), captionFrame().bottom()  - ( textHeight(*p) / 2) ) , getCaption() );
+
+    setFontAttribute(10, 1.0, false, QColor(0,0,0), p);
+    p->drawText( QPointF( captionFrame().right() - textWidth(getCurrentDate() , *p) - pixel(3),
+                               captionFrame().bottom()  - ( textHeight(*p) / 2) ), getCurrentDate() );
+
+    // Print page feet information
+    setFontAttribute(8, 1.0, false, Qt::darkGray, p);
+
+    QString fileino = getDocName();
+    if(getProject().isValid())                          // + ":" + getFilepath(); not needed
+        fileino.append(":"+getProject().createTime());
+    qreal w = textWidth(fileino, *p);
+    p->drawText( QPointF( feetFrame().x() + 10, feetFrame().bottom() - ( textHeight(*p) / 2) ) , fileino );
+
+
+    QString cp = QString::number(currentPage,10);
+    QString mp = QString::number(maxPage,10);
+    QString pageCount = "Seite " + cp + " von " + mp;
+
+    qreal posX = w + pixel(10);
+    if(posX < feetFrame().width() / 2 - (textWidth(pageCount, *p) / 2))
+        posX = feetFrame().width() / 2 - (textWidth(pageCount, *p) / 2);
+
+    p->drawText( QPointF( posX , feetFrame().bottom() - ( textHeight(*p) / 2) ) , pageCount );
+
+}
+
+void ClassPrinting::printProjectInfoText(QPainter *p)
+{
+    // Print project base info
+    QPointF row1 = rowMap.value(0);
+    p->drawText(row1, getProject().getKey());
+    QString dt = "Datum:";
+    QRectF rdt( pageFrame().right() - pixel(23), row1.y() - pixel(4),
+                  pixel(20), pixel(7));
+    p->drawText( pageRect().right() - rdt.width() - textWidth(dt, *p) - pixel(6), row1.y(), dt);
+
+    setFontAttribute(10, 1, false, Qt::darkGray, p);
+    p->drawRect( rdt);
+
+    // Print project description
+    setFontAttribute(10, 1, false, QColor(0,0,0), p);
+    QPointF row2 = rowMap.value(1);
+    p->drawText( row2, getDescription());
+
+    // Print a rectangle for skill number
+    QPointF row3 = rowMap.value(2);
+    setFontAttribute(10, 1, false, Qt::darkGray, p);
+    QRectF rnr( pageFrame().right() - pixel(23), row3.y() - pixel(5) - (textHeight(*p)/2),
+                  pixel(20), pixel(12));
+    p->drawRect( rnr );
+
+    setFontAttribute(10, 1, true, QColor(0,0,0), p);
+    QString pn = "Prüfungs-Nr:";
+    row3.setX(pageFrame().right() - rnr.width() - textWidth(pn, *p) - pixel(6));
+    p->drawText(row3 , pn);
+
+    // Print column for editing the points
+    setFontAttribute(10, 1, false, QColor(0,0,0), p);
+    QString mp = "MaxPunkte";
+    columnHeaderText.insert(3, mp);
+
+    QString p1 = "Prüfer 1";
+    columnHeaderText.insert(2, p1);
+
+    QString p2 = "Prüfer 2";
+    columnHeaderText.insert(1, p2);
+
+    QString p3 = "Prüfer 3";
+    columnHeaderText.insert(0, p3);
+
+    QPointF colPos = rowMap.value(4);
+    qreal width = pageRect().right();
+
+    colPos.setX( width );
+    for(int i = 0; i < columnHeaderText.size(); i++)
+    {
+        qreal wt = textWidth( columnHeaderText.value(i), *p) + pixel(3);
+        colPos.setX( colPos.x() - wt );
+        columnPos.insert(i, colPos);
+        p->drawText( colPos, columnHeaderText.value(i));
+    }
+}
+
+void ClassPrinting::printProjectText(QPainter *p)
+{
+
+    int rowNr = 5;
+
+    // Calculate the maximal length of row
+    QPointF pos1 = rowMap.value(0);
+    pos1.setY(0);
+    QPointF pos2 = columnPos.last();
+    pos2.setY(0);
+    qreal maxRowLength = QLineF(pos1, pos2).length() - textWidth("100.", *p) - pixel(5);
+
+
+    QMap<int, ClassFrage> qMap;
+    qMap = getProject().questionMap();
+
+    QMapIterator<int, ClassFrage> it(qMap);
+    while (it.hasNext())
+    {
+        it.next();
+
+        setFontAttribute(10, 1.0, false, QColor(0,0,0), p);
+
+        QString qu = it.value().question();
+        if(!it.value().identifier().isEmpty())
+            qu.append(" : " + it.value().identifier());
+
+        QString nr = QString::number(it.value().questionNr(), 10)+".";
+        QString mp = QString::number( it.value().maxPoints(), 10);
+
+        // Print number of question
+        QPointF pos = rowMap.value( rowNr );
+        p->drawText(pos, nr);
+
+        // Print question split when string to long
+        QStringList sList = splitText(qu, maxRowLength , *p);
+        foreach (QString s, sList)
+        {
+            pos = rowMap.value(rowNr);
+            pos.setX( pos.x() + textWidth("100.", *p) );
+            p->drawText(pos, s);
+
+            if(sList.first() == s)
+            {
+                // Print max points
+                pos.setX( columnPos.last().x() );
+                p->drawText( pos, mp );
+
+                // Definition of edit rect for points
+                setFontAttribute(10, 1, false, Qt::darkGray, p);
+                for(int i = 0; i < columnHeaderText.size()-1; i++)
+                {
+                    QRectF editRect( columnPos.value(i).x(),
+                                     pos.y() - (textHeight(*p)/2) - pixel(2),
+                                     textWidth(columnHeaderText.value(i), *p),
+                                     pixel(6));
+
+                    p->drawRect( editRect );
+                }
+                setFontAttribute(10, 1, false, QColor(0,0,0), p);
+            }
+
+            if(sList.last() != s)
+                rowNr++;
+        }
+
+        if(rowNr > rowMap.size()-2)
+        {
+            if(setupNewPage(p))
+                rowNr = 5;
+            else
+                ; // emit error
+
+        }
+        else
+            rowNr++;
+    }
+
+    // Print the last rows
+    rowNr++;
+    if(rowNr > rowMap.size() - 2)
+    {
+        setupNewPage(p);
+        rowNr = 5;
+    }
+
+    QPointF pos = rowMap.value(rowNr);
+    QString result = "Ergebnis:";
+    p->drawText( rowMap.value(rowNr), result);
+
+    setFontAttribute(10, 1, false, Qt::darkGray, p);
+    qreal rectWidth = 0;
+    for(int i = 0; i < columnHeaderText.size()-1; i++)
+    {
+        QRectF editRect( columnPos.value(i).x(),
+                         pos.y() - (textHeight(*p)/2) - pixel(2),
+                         textWidth(columnHeaderText.value(i), *p),
+                         pixel(6));
+
+        p->drawRect( editRect );
+        rectWidth = editRect.width();
+    }
+
+    rowNr++;
+    rowNr++;
+    if(rowNr > rowMap.size() - 2)
+    {
+        setupNewPage(p);
+        rowNr = 5;
+    }
+
+    pos = rowMap.value(rowNr);
+    setFontAttribute(10, 1, true, QColor(0,0,0), p);
+    QString total = "Gesamt:";
+    p->drawText( rowMap.value(rowNr), total);
+
+    setFontAttribute(10, 1, true, Qt::darkGray, p);
+    QRectF editRect( columnPos.value(2).x(),
+                     pos.y() - (textHeight(*p)/2) - pixel(2),
+                     rectWidth * 3 + pixel(6),
+                     pixel(6));
+
+    p->drawRect( editRect );
+
+}
+
+void ClassPrinting::printApprenticeList(QPainter *p)
+{
+    QMap<int, qreal> columnWidth;
+
+    columnHeaderText.insert(0, "Nr.");
+    columnWidth.insert(0, pixel(20));
+
+    columnHeaderText.insert(1, "Name");
+    columnWidth.insert(1, pixel(45));
+
+    columnHeaderText.insert(2, "Klasse");
+    columnWidth.insert(2, pixel(20));
+
+    columnHeaderText.insert(3, "Adresse");
+    columnWidth.insert(3, pixel(75));
+
+    columnHeaderText.insert(4, "Telefon");
+    columnWidth.insert(4, pixel(40));
+
+    columnHeaderText.insert(5, "Betrieb");
+    columnWidth.insert(0, pixel(10));
+
+
+    int rowNr = 0;         // number of row
+    qreal xPos = pixel(3); // start position
+    QPointF pos;           // position of current painter
+
+    pos.setY( rowMap.value(rowNr).y() );
+    pos.setX( xPos );
+    for(int i = 0; i < columnHeaderText.size(); i++)
+    {
+        QString s = columnHeaderText.value(i);
+        p->drawText( pos, s);
+        columnPos.insert(i, pos);
+        xPos += columnWidth.value(i);
+        pos.setX( xPos );
+    }
+
+
+    foreach (ClassLehrling appr, getApprenticeList())
+    {
+        rowNr++;
+        QString nr = QString::number(appr.nr(),10);
+        QString name = appr.firstname() + " " + appr.surname();
+        QString klasse = appr.educationClass();
+        QString betrieb = appr.company();
+        QString adress = appr.street() +"," + appr.city();
+        QString telefon = appr.phone();
+
+        QPointF pos = rowMap.value(rowNr);
+
+        pos.setX(columnPos.value(0).x());
+        p->drawText( pos, nr);
+
+        pos.setX(columnPos.value(1).x());
+        p->drawText( pos, name);
+
+        pos.setX(columnPos.value(2).x());
+        p->drawText( pos, klasse);
+
+        pos.setX(columnPos.value(3).x());
+        p->drawText( pos, adress);
+
+        pos.setX(columnPos.value(4).x());
+        p->drawText( pos, telefon);
+
+        pos.setX(columnPos.value(5).x());
+        p->drawText( pos, betrieb);
+
+        if(rowNr > rowMap.size() - 2)
+        {
+            setupNewPage(p);
+            rowNr = 0;
+        }
+    }
+
+}
+
+bool ClassPrinting::setupNewPage(QPainter *p)
+{
+    if(printer.newPage())
+    {
+        printPageFrames(p);
+        printPageInfoText(p);
+
+        if(getProject().isValid())
+            printProjectInfoText(p);
+
+        return true;
+    }
+
+    return false;
+}
--- classprinting.h
+++ classprinting.h
@@ -9,13 +9,18 @@
 #include <QMarginsF>
 #include <QMap>
 
+#include <classprojekt.h>
+#include <classskills.h>
+#include <classlehrling.h>
+
 class ClassPrinting : public QObject
 {
     Q_OBJECT
 public:
     explicit ClassPrinting(QObject *parent = nullptr);
 
-    bool setupPrinter();
+    bool setupPrinter(QPageLayout::Orientation layout);
+
     void setText(const QString &value);
     QString getText() const;
 
@@ -34,11 +39,16 @@ public:
     QString getDescription() const;
     void setDescription(const QString &value);
 
-    void setColumn(int nr, qreal width);
-    void setColumnText(int nr, const QString &text);
-    void setColumnRect(int nr, const QRectF &rect);
-
     bool print();
+
+    ClassProjekt getProject() const;
+    void setProject(const ClassProjekt &project);
+
+    QString getFilepath() const;
+    void setFilepath(const QString &value);
+
+    QList<ClassLehrling> getApprenticeList() const;
+    void setApprenticeList(const QList<ClassLehrling> &apprenticeList);
 
 signals:
     void aborted();
@@ -52,32 +62,56 @@ private:
     QString projectName;
     QString createdDate;
     QString description;
+    QString filepath;
 
     QPrinter printer;
     QMarginsF margin;
+    int maxPage;
+    int currentPage;
+    qreal originPenSize;
 
-    QMap<int, qreal> columnMap;
-    QMap<int, QString> columnText;
-    QMap<int, QRectF> columnRect;
+    QMap<int, QPointF> rowMap;
+    void setupRowMap(const QPainter &p);
 
+    ClassProjekt m_project;
+    QMap<int, QPointF> columnPos;
+    QMap<int, QString> columnHeaderText;
+
+
+    QList<ClassLehrling> m_apprenticeList;
 
     qreal pageLeft();
+    qreal pageRight();
     qreal pageTop();
     qreal pageBottom();
     qreal pageWidth();
     qreal pageHeight();
+    QRectF pageRect();
 
-    QRectF headerFrame();
+    QRectF captionFrame();
     QRectF feetFrame();
     QRectF pageFrame();
 
     QString getCurrentDate();
 
-    qreal pixel(qreal millimeter, const QPrinter &p);
-    void setFontAttribute(int size, bool bold, QColor col, QPainter *p);
+    qreal pixel(qreal millimeter);
+    void setFontAttribute(int fontSize, qreal penWidth, bool bold, QColor col, QPainter *p);
 
     qreal textHeight(const QPainter &p);
     qreal textWidth(const QString &text, const QPainter &p);
+    void setPenSize(qreal size, QPainter *p);
+
+
+    QStringList splitText(const QString &text, qreal maxLength, const QPainter &p);
+    int calculateRows(const QStringList &textList, qreal rowWidth, const QPainter &p);
+    QStringList textList(const ClassProjekt &pro);
+
+    void printPageFrames(QPainter *p);
+    void printPageInfoText(QPainter *p);
+    void printProjectInfoText(QPainter *p);
+    void printProjectText(QPainter *p);
+    void printApprenticeList(QPainter *p);
+    bool setupNewPage(QPainter *p);
 
 };
 
